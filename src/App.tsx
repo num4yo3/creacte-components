96b400be483e7a6ed1fb3a0d77ff6d18df67eed7
num4yo3 <82578883+num4yo3@users.noreply.github.com>
2022-01-04 13:16:15 +0900

    GridComponent完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import "./styles.css";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { GridComponent, GridItem } from "./components/GridComponent";
+
+const config = {
+  "fast-preview": {
+    disabled: true
+  },
+  tex2jax: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"]
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"]
+    ]
+  },
+  messageStyle: "none"
+};
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <MathJaxContext version={2} config={config}>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <MathJax style={{ fontSize: "20px" }}>{`$\\int_0^x a^4$`}</MathJax>
+
+        <GridComponent ratio={[3, 2, 1]} height={300} guid={true}>
+          <GridItem n={1}>
+            <div style={{ height: "100%" }}>Apple</div>
+          </GridItem>
+          <GridItem n={2}>
+            <div>orange</div>
+          </GridItem>
+          <GridItem n={3}></GridItem>
+        </GridComponent>
+        <GridComponent ratio={[1, 2]} height={300} guid={true}>
+          <GridItem n={1}></GridItem>
+          <GridItem n={2}></GridItem>
+        </GridComponent>
+      </MathJaxContext>
+    </>
   );
 }
---
+++ src/components/GridComponent.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+type Style = {
+  ratio: number[];
+  height?: number;
+  guid?: boolean;
+};
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: row; /* row, row-reverse, column, column-reverse */
+  flex-wrap: nowrap; /* nowrap, wrap, wrap-reverse */
+  justify-content: center; /* flex-start, flex-end, center, space-between, space-arround */
+  align-items: center; /* stretch, flex-start, flex-end, center, baseline */
+  align-content: center; /* stretch, flex-start, flex-end, center, space-between, space-arround */
+  width: 100%;
+  height: ${(props) => (props.height ? props.height + "px" : "100%")};
+  ${(props) => props.guid && "outline: dotted 4px silver;"}
+
+  .item-01 {
+    flex-grow: ${(props) => props.ratio[0]};
+    height: 100%;
+    ${(props) => props.guid && "outline: solid 1px skyblue;"}
+  }
+  .item-02 {
+    flex-grow: ${(props) => props.ratio[1]};
+    height: 100%;
+    ${(props) => props.guid && "outline: solid 1px skyblue;"}
+  }
+  .item-03 {
+    flex-grow: ${(props) => props.ratio[2]};
+    height: 100%;
+    ${(props) => props.guid && "outline: solid 1px skyblue;"}
+  }
+  .item-04 {
+    flex-grow: ${(props) => props.ratio[3]};
+    height: 100%;
+    ${(props) => props.guid && "outline: solid 1px skyblue;"}
+  }
+  .item-05 {
+    flex-grow: ${(props) => props.ratio[4]};
+    height: 100%;
+    ${(props) => props.guid && "outline: solid 1px skyblue;"}
+  }
+`;
+
+export const GridComponent: React.FC<Props> = (props: Style) => {
+  return (
+    <Root height={props.height} ratio={props.ratio} guid={props.guid}>
+      {props.children}
+    </Root>
+  );
+};
+
+export const GridItem: React.FC<Props> = (props) => {
+  return (
+    <div
+      className={"item-0" + props.n}
+      style={props.height && { height: props.height }}
+    >
+      {props.children}
+    </div>
+  );
+};
